22bf23fbacc4e6a0a70c3cb9b86051337182faab
Quang Dao <quangdao@itrvn.com>
2021-05-14 07:39:44 +0700

    feat: :sparkles: insight
    create spend, get insight

--- src/resolvers/insight.js
+++ src/resolvers/insight.js
@@ -4,7 +4,7 @@ export default {
   Query: {
     insight: async (parent, { from, to }, { models, me }) => {
       const expenditures = await models.Expenditure.find({
-        user: me._id,
+        user: me.id,
         date: { $gt: from, $lt: to },
       });
       const charts = [];
@@ -14,14 +14,14 @@ export default {
       const diffTime = Math.abs(+from - +to);
       const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
       for (let i = 0; i < diffDays; i += 1) {
-        const day = from + i * (1000 * 60 * 60 * 24);
-        const chart = 0;
-        const expenditureInDay = _.filter(expenditures, (e) => +e.date < day);
+        const day = +from + i * (1000 * 60 * 60 * 24);
+        const dayPlus1 = +from + (i + 1) * (1000 * 60 * 60 * 24);
+        const expenditureInDay = _.filter(expenditures, (e) => +e.date > day && +e.date < +dayPlus1);
         const totalInDay = _.sumBy(expenditureInDay, (e) => e.spending);
         total += totalInDay;
         if (max < totalInDay) max = totalInDay;
         if (min > totalInDay) min = totalInDay;
-        charts.push(chart);
+        charts.push(totalInDay);
       }
       return { charts, total, max, min, average: Math.ceil(total / diffDays) };
     },
